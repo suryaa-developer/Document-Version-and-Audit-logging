b9fd47910a9ee635d211c7421647466501978dac
suryaa-developer <suryaarul2001@gmail.com>
2026-02-27 23:11:08 +0530

    feat : get authenticated current user

--- DocAudit/src/main/java/com/Document/DocAudit/securityConfig/SecurityConfig.java
+++ DocAudit/src/main/java/com/Document/DocAudit/securityConfig/SecurityConfig.java
@@ -4,6 +4,9 @@ import com.Document.DocAudit.service.UserOAuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,6 +31,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    public static String GetCurrentUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth != null ? auth.getName():null;
     }
 }
 
