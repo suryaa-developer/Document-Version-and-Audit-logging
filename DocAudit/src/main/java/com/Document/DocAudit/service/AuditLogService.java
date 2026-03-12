48eff4cb567c72b2cd4d35cdf7921dfe77474bef
suryaa-developer <suryaarul2001@gmail.com>
2026-03-12 23:03:50 +0530

    chore : remove import

---
+++ DocAudit/src/main/java/com/Document/DocAudit/service/AuditLogService.java
@@ -0,0 +1,29 @@
+package com.Document.DocAudit.service;
+
+import com.Document.DocAudit.entity.AuditLog;
+import com.Document.DocAudit.entity.UserEntity;
+import com.Document.DocAudit.enums.DocumentAction;
+import com.Document.DocAudit.repository.AuditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AuditLogService {
+    @Autowired
+    AuditRepository auditRepository;
+
+    public void CreateDocumentLog(Long docId, DocumentAction docAction, UserEntity user,
+                                  String previousContent, String newContent){
+        AuditLog log = new  AuditLog();
+        log.setDocumentId(docId);
+        log.setAction(docAction);
+        log.setPreviousContent(previousContent);
+        log.setNewContent(newContent);
+        log.setModifiedBy(user);
+        log.setModifiedAt(LocalDateTime.now());
+
+        auditRepository.save(log);
+    }
+}
