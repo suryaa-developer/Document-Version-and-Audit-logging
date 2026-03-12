8b29e0880447102d5a1e5ea7877f110e6493b7c2
suryaa-developer <suryaarul2001@gmail.com>
2026-03-12 23:10:12 +0530

    fixed filteration search and added added validations

--- DocAudit/src/main/java/com/Document/DocAudit/service/DocumentService.java
+++ DocAudit/src/main/java/com/Document/DocAudit/service/DocumentService.java
@@ -2,6 +2,7 @@ package com.Document.DocAudit.service;
 
 import com.Document.DocAudit.entity.Document;
 import com.Document.DocAudit.entity.UserEntity;
+import com.Document.DocAudit.enums.DocumentAction;
 import com.Document.DocAudit.enums.DocumentStatus;
 import com.Document.DocAudit.repository.DocumentRepository;
 import com.Document.DocAudit.repository.UserRepository;
@@ -14,6 +15,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DocumentService {
@@ -21,13 +24,12 @@ public class DocumentService {
     private DocumentRepository documentRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AuditLogService auditLogService;
 
     @Transactional
     public Document CreateDocument(String title,String Content){
-        System.out.println("Attempting to create document: " + title+" content: " + Content);
-
        String currentUser = SecurityConfig.GetCurrentUser();
-        System.err.println("currentUser:"+currentUser);
        if(currentUser == null){
            throw new IllegalArgumentException("You need to be logged in first");
        }
@@ -36,7 +38,7 @@ public class DocumentService {
        }
 
         // Change findByname to findByEmail
-        UserEntity creator = userRepository.findByProviderId(currentUser)
+        UserEntity creator = userRepository.findByEmail(currentUser)
                 .orElseThrow(() -> new RuntimeException("User not found with email: " + currentUser));
 
        Document newDoc = new Document();
@@ -62,17 +64,20 @@ public class DocumentService {
         }
 
         String currentUser = auth.getName();
-        UserEntity user = userRepository.findByname(currentUser)
+        UserEntity user = userRepository.findByEmail(currentUser)
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
 
         // Load document
         Document doc = documentRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Document not found"));
 
+        String PreviousContent = doc.getContent();
         if (doc.getCreatedBy().equals(user)) {
             doc.setContent(content);
             doc.setUpdatedAt(LocalDateTime.now()); // or use @LastModifiedDate
-            return documentRepository.save(doc);
+            documentRepository.save(doc);
+            auditLogService.CreateDocumentLog(id, DocumentAction.UPDATED,user,PreviousContent,content);
+            return doc;
         } else {
             throw new AccessDeniedException("You don't have permission to edit this document");
         }
@@ -82,9 +87,33 @@ public class DocumentService {
     public void DeleteDocument(Long id) {
         Document doc = documentRepository.findById(id).
                 orElseThrow(()->new EntityNotFoundException("Document not found"));
-        doc.setStatus(DocumentStatus.DELETED);
-        doc.setUpdatedAt(LocalDateTime.now());
-        documentRepository.save(doc);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new AccessDeniedException("You must be logged in to edit a document");
+        }
+
+        String currentUser = auth.getName();
+        UserEntity user = userRepository.findByEmail(currentUser)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        if (doc.getCreatedBy().equals(user)) {
+            doc.setStatus(DocumentStatus.DELETED);
+            doc.setUpdatedAt(LocalDateTime.now());
+            documentRepository.save(doc);
+        }else{
+            throw new AccessDeniedException("You don't have permission to delete this document");
+        }
+    }
+
+    // Get all active documents
+    public List<Document> getActiveDocuments() {
+        return documentRepository.findByStatus(DocumentStatus.ACTIVE);
+    }
+
+    // Get active document by ID
+    public Optional<Document> getActiveDocumentById(Long id) {
+        return documentRepository.findById(id)
+                .filter(doc -> DocumentStatus.ACTIVE.equals(doc.getStatus()));
     }
 
 
