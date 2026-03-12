fbaa36d3149da66699b7b1ce94e4f20fa758ddd0
suryaa-developer <suryaarul2001@gmail.com>
2026-03-12 22:52:24 +0530

    feat : added find by status method

--- DocAudit/src/main/java/com/Document/DocAudit/repository/DocumentRepository.java
+++ DocAudit/src/main/java/com/Document/DocAudit/repository/DocumentRepository.java
@@ -1,7 +1,10 @@
 package com.Document.DocAudit.repository;
 
 import com.Document.DocAudit.entity.Document;
+import com.Document.DocAudit.enums.DocumentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface DocumentRepository extends JpaRepository<Document,Long> {
+    List<Document> findByStatus(DocumentStatus status);
 }
