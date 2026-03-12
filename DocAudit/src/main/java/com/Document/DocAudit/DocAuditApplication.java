f91853be27e36508496ace214d74c2077bfd455f
suryaa-developer <suryaarul2001@gmail.com>
2026-03-12 23:13:01 +0530

    fix : added jpa auditing

--- DocAudit/src/main/java/com/Document/DocAudit/DocAuditApplication.java
+++ DocAudit/src/main/java/com/Document/DocAudit/DocAuditApplication.java
@@ -2,8 +2,10 @@ package com.Document.DocAudit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class DocAuditApplication {
 
 	public static void main(String[] args) {
