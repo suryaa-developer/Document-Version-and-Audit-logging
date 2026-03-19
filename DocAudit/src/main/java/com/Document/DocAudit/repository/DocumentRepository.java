74d9236b5788479fb14dc89302c997931878093d
suryaa-developer <suryaarul2001@gmail.com>
2026-03-19 22:52:20 +0530

    perf : change list to pagination in repo

--- DocAudit/src/main/java/com/Document/DocAudit/repository/DocumentRepository.java
+++ DocAudit/src/main/java/com/Document/DocAudit/repository/DocumentRepository.java
@@ -2,9 +2,10 @@ package com.Document.DocAudit.repository;
 
 import com.Document.DocAudit.entity.Document;
 import com.Document.DocAudit.enums.DocumentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface DocumentRepository extends JpaRepository<Document,Long> {
-    List<Document> findByStatus(DocumentStatus status);
+    Page<Document> findByStatus(DocumentStatus status, Pageable pageable);
 }
