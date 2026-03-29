1f8aa12c7a23101562b1e46cee87b7c32bc2fbf9
suryaa-developer <suryaarul2001@gmail.com>
2026-03-29 18:30:38 +0530

    refactor : make api response standard and removed inwanted methods

--- DocAudit/src/main/java/com/Document/DocAudit/controller/UserController.java
+++ DocAudit/src/main/java/com/Document/DocAudit/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.Document.DocAudit.controller;
 
+import com.Document.DocAudit.dto.ApiResponse;
 import com.Document.DocAudit.dto.UserResponse;
 import com.Document.DocAudit.entity.UserEntity;
 import com.Document.DocAudit.securityConfig.CustomUserPrincipal;
 import com.Document.DocAudit.securityConfig.JwtUtils;
 import com.Document.DocAudit.securityConfig.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,11 @@ public class UserController {
     JwtUtils jwtUtils;
 
     @GetMapping("/usercreate")
-    public UserResponse GetUser(@AuthenticationPrincipal CustomUserPrincipal principal){
+    public ResponseEntity<ApiResponse<UserResponse>> GetUser(@AuthenticationPrincipal CustomUserPrincipal principal){
         UserEntity user = principal.getUser();
         // 1. Generate the token specifically for this logged-in user
         String jwtToken = jwtUtils.generateToken(user.getName());
-        return new UserResponse(
+        UserResponse userResponse= new      UserResponse(
                 user.getUserId(),
                 user.getEmail(),
                 user.getName(),
@@ -32,16 +34,16 @@ public class UserController {
                 user.getStatus(),
                 jwtToken
         );
+        ApiResponse<UserResponse> response =
+                new ApiResponse<>(true, "User fetched successfully", userResponse);
+
+        return ResponseEntity.ok(response);
     }
     @GetMapping("/success")
-    public String LoginSuccess(){
+    public ResponseEntity<ApiResponse<String>> LoginSuccess(){
         String Username = SecurityConfig.GetCurrentUser();
-        return Username +" Login Success";
+        return ResponseEntity.ok(
+                new ApiResponse<>(true, "Login successful", Username)
+        );
     }
-
-    @GetMapping("/api/secure-data")
-    public String secureData() {
-        return "Sensitive info";
-    }
-
 }
