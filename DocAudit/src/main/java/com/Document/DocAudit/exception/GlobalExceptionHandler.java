5bfaae198a136dacc7663295c6081bcf65f713ed
suryaa-developer <suryaarul2001@gmail.com>
2026-03-29 18:36:42 +0530

    feat : centralised Exception class

---
+++ DocAudit/src/main/java/com/Document/DocAudit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,61 @@
+package com.Document.DocAudit.exception;
+
+import com.Document.DocAudit.dto.ApiResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.security.access.AccessDeniedException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Object>> handleIllegalArgumentException(IllegalArgumentException e){
+        ApiResponse<Object> response =
+                new ApiResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleEntityNotException(EntityNotFoundException e){
+        ApiResponse<Object> response =
+                new ApiResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAccessDeniedException(AccessDeniedException e){
+        ApiResponse<Object> response =
+                new ApiResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(OAuth2AuthenticationException.class)
+    public ResponseEntity<ApiResponse<Object>> handleOAuthException(OAuth2AuthenticationException e){
+        ApiResponse<Object> response =
+                new ApiResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMethodException(MethodArgumentNotValidException e){
+
+        String message = ( e.getAllErrors().get(0)).getDefaultMessage();
+        ApiResponse<Object> response =
+                new ApiResponse<>(false,message, null);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleException(Exception e){
+        ApiResponse<Object> response =
+                new ApiResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
