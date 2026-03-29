9eef08b701ac4eabbeb25276c2029d8736677ae8
suryaa-developer <suryaarul2001@gmail.com>
2026-03-29 18:33:24 +0530

    refactor : added validations

--- DocAudit/src/main/java/com/Document/DocAudit/dto/DocumentRequest.java
+++ DocAudit/src/main/java/com/Document/DocAudit/dto/DocumentRequest.java
@@ -1,9 +1,14 @@
 package com.Document.DocAudit.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class DocumentRequest {
+    @NotBlank(message = "Title must not be empty")
     private String title;
+    @NotBlank(message = "Content must not be empty")
+    @Size(max = 5000,message = "Content should not exceed 5000 characters")
     private String content;
 }
