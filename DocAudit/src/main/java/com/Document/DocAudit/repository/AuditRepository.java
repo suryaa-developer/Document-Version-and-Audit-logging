14c37960f3ee1c1ecf467454d8aa9600b8148f14
suryaa-developer <suryaarul2001@gmail.com>
2026-04-10 22:49:29 +0530

    feat : added find by doc id method

--- DocAudit/src/main/java/com/Document/DocAudit/repository/AuditRepository.java
+++ DocAudit/src/main/java/com/Document/DocAudit/repository/AuditRepository.java
@@ -1,7 +1,10 @@
 package com.Document.DocAudit.repository;
 
 import com.Document.DocAudit.entity.AuditLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuditRepository extends JpaRepository<AuditLog,Long> {
+    Page<AuditLog> findAllByDocumentId(Pageable pageable,Long documentId);
 }
